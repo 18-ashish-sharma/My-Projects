b3161acc6cdbe60682e5da6600af32e2182e722e
ashish <hudasharma10@gmail.com>
2021-05-15 19:37:23 +0530

    Finished some wrk to be done

---
+++ new/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static('code'))
+
+app.get('/', async (req, res) => {
+    res.sendFile('./code/index.html')
+})
+
+app.listen(port , (req, res) => console.log('Server Started'))
