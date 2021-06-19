29ad50074df46bff1f9e874ca74dd9e9f84fead4
ashish <hudasharma10@gmail.com>
2021-06-19 18:52:34 +0530

    reactto-do

---
+++ React ToDo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Todos from "./Todo";
+
+function App () {
+  return(
+  <div>
+    <Todos/>
+    <h4>Code By</h4>
+    <a href="https://www.linkedin.com/in/ashish-sharma-aa39711b4/?locale=en_US">ASHISH SHARMA</a>
+    
+  </div>
+  )
+}
+
+export default App;
